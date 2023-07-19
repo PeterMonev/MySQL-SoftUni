bc82298c76259ff54ab4cc26d65d0b3689d7e713
PeterMonev <monevpeter@gmail.com>
2023-07-19 23:27:41 +0300

    Create 1. Managers.sql

---
+++ Subqueries and JOINs - Lab/1. Managers.sql
@@ -0,0 +1,3 @@
+SELECT e.employee_id, concat_ws(' ', first_name, last_name) AS 'full_name', d.department_id, d.name FROM employees AS e
+JOIN departments AS d ON d.manager_id = e.employee_id
+ORDER BY e.employee_id LIMIT 5;
